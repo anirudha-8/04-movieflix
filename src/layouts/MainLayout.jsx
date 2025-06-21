05653f10a3cb4174abbd67d878ca32a086840165
Anirudha Bele <beleanirudha8@gmail.com>
2025-06-21 12:46:36 +0530

    feat: create MainLayout with Navbar and Outlet for nested routing

--- src/layouts/MainLayout.jsx
+++ src/layouts/MainLayout.jsx
@@ -1,4 +1,14 @@
+import { Outlet } from "react-router-dom";
+import Navbar from "../components/Navbar";
+
 const MainLayout = () => {
-	return <div>MainLayout</div>;
+	return (
+		<>
+			<Navbar />
+			<main className="max-w-6xl mx-auto px-4 py-6">
+				<Outlet />
+			</main>
+		</>
+	);
 };
 export default MainLayout;
