70569104586d1ff939ce22c03294ef6cd51e6d9d
Anirudha Bele <beleanirudha8@gmail.com>
2025-06-21 20:12:11 +0530

    feat: create Home page layout with intro and trending section placeholder

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,4 +1,18 @@
 const Home = () => {
-	return <div>Home</div>;
+	return (
+		<div>
+			<div>
+				<h1>Welcome to MovieFlix</h1>
+				<p>Your source for trending and popular movies!</p>
+				<button>Explore Now</button>
+			</div>
+			<div>
+				<img
+					src="https://t4.ftcdn.net/jpg/12/49/96/91/240_F_1249969193_ytco7i8sAdBLPtG1LaLbjhJxaWfDDTvE.jpg"
+					alt="movieflix hero image"
+				/>
+			</div>
+		</div>
+	);
 };
 export default Home;
